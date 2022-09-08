13c42a87cd3422f375f302614c1abab869d537aa
Kanem <kanemdeng@email.com>
2022-09-08 01:09:00 -0700

    asynce thunks

--- toolkit/app/store.js
+++ toolkit/app/store.js
@@ -3,13 +3,15 @@ const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/icecream/icecreamSlice')
 // const reduxLogger = require('redux-logger')
 const { getDefaultMiddleware } = require('@reduxjs/toolkit')
+const userReducer = require('../features/user/userSlice')
 
 // const logger = reduxLogger.createLogger()
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
-    icecream: icecreamReducer
+    icecream: icecreamReducer,
+    user: userReducer
   },
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
